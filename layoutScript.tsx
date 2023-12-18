cb264613f360ce102479430e22c1467d1064b224
houhuabin <houhuabin@163.com>
2023-12-19 12:51:42 +1300

    seperate google analytics script to layoutScript

---
+++ layoutScript.tsx
@@ -0,0 +1,28 @@
+import Script from "next/script"
+
+import React from 'react'
+
+const LayoutScript = () => {
+    return (
+        <><GoogleAnalyticsScript /></>
+    )
+}
+
+export default LayoutScript
+
+const GoogleAnalyticsScript = () => {
+    return (
+        <>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-RFNX3HTB3Y" />
+            <Script>
+                {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', 'G-RFNX3HTB3Y');`}
+            </Script>
+        </>
+    )
+
+}
+
